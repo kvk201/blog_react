051cb8fb136da6b31594e91a1dcf9fb5268db4d3
messi618 <54788160+messi618@users.noreply.github.com>
2023-05-15 11:56:38 -0500

    custom hook added

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,18 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending,setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+    //data: blogs... means grab the data from the useFetch custom hook but call it blogs instead
 
+    
     //use effects runs everytime the component re-renders.. it takes a function as arg and this fn runs everytime comp rerenders
     useEffect(()=>{
         console.log('use effect ran on each render and re-render');
     });
-    //best place to fetch data
-    useEffect(()=>{
-        console.log('use effect ran only once during intialization..');
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogss')
-                .then(res=>{
-                    if (!res.ok){
-                        throw Error('could not fetch data for resource')
-                    }
-                    return res.json();
-                })
-                .then((data)=>{
-                    console.log(data);
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err=>{
-                    setError(err.message)
-                    setIsPending(false)
-                });
-                }, 1000);
-        
-    }, []);
+    
 
     useEffect(()=>{
         console.log('use effect ran on render and re-render of blog');
@@ -45,8 +23,8 @@ const Home = () => {
         <div className="home">
             { error && <div>{error}</div>}
             { isPending && <div>loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs"/>/*conditional template, only if left is true right template will be outputted*/}
-            {blogs && <BlogList blogs={blogs.filter((blog)=>blog.author==='vivek')} title="Vivek's Blogs"/>}
+            { blogs && <BlogList blogs={blogs} title="All Blogs"/>/*conditional template, only if left is true right template will be outputted*/}
+            { blogs && <BlogList blogs={blogs.filter((blog)=>blog.author==='vivek')} title="Vivek's Blogs"/>}
         </div>
      );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,4 +1,36 @@
-// custom hook need to start with keyword use
-const useFetch = () => {
+import { useState, useEffect } from "react";
 
+// custom hook need to start with keyword use
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending,setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+     //best place to fetch data
+    useEffect(()=>{
+        console.log('use effect ran only once during intialization..');
+        setTimeout(() => {
+            fetch(url)
+                .then(res=>{
+                    if (!res.ok){
+                        throw Error('could not fetch data for resource')
+                    }
+                    return res.json();
+                })
+                .then(data=>{
+                    console.log(data);
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err=>{
+                    setError(err.message)
+                    setIsPending(false)
+                });
+                }, 1000);
+        
+    }, [url]);
+
+    return { data, isPending, error}
 }
+export default useFetch;
